66028c315cc116fe828acde8ce79bb76af6fe431
Mateus Moura <chagas.mateus@gmail.com>
2015-07-16 23:53:47 -0300

    development block__highlights

--- prototipo/index.php
+++ prototipo/index.php
@@ -1,8 +1,8 @@
 <?php include("inc/header.php"); ?>
 
 		<main role="main" class="home">
-			<section class="block__post">
-				<div class="center">
+			<div class="center">
+				<section class="block__post">
 					<div class="row">
 						<div class="block__post--title">
 							<h1>Há flores por todos os lados</h1>
@@ -32,187 +32,193 @@
 							</div>
 						</div>
 					</div>
-				</div>
-			</section>
-
-			<div class="block__adsence--full">
-				<img src="img/fke/adsence-full.png" alt="Publicidade Full">
+				</section>
 			</div>
 
-			<div class="block__highlights">
-				<div class="row">
-					<div class="col-m-4">
-						<div class="block__adsence--medium">
-							<img src="img/fke/adsence-medium.png" alt="Publicidade Media">
-						</div>
-					</div>
-
-					<div class="col-m-6">
-						<div class="block__post--highlights">
-							<div class="block__post--image">
-								<img src="img/fke/post-image-1.png" alt="Quinta é quase sexta">
-							</div>
-
-							<div class="block__post--category">
-								<a href="#this" class="btn btn-default">bebes</a>
-							</div>
-
-							<div class="block__post--content">
-								<div class="block__post--share">
-									<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
-									<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
-								</div>
-
-								<div class="block__post--title">
-									<h2>Quinta é quase sexta</h2>
-								</div>
-
-								<div class="block__post--time">
-									<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>danicronemberger</strong></a></p>
-								</div>
-
-								<div class="block__post--entries">
-									<p>Em primeiro lugar, permita-me discorrer sobre o conceito que cunhei sobre música-boa. Segundo eu mesma, música-boa é aquela de que você gosta. Ponto. Aquela que você cresceu ouvindo. Ponto. Segundo eu mesma, música-boa é aquela de que você gosta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
-								</div>
-							</div>
-						</div>
+			<div class="center">
+				<div class="block__adsence--full">
+					<div class="align-center">
+						<img src="img/fke/adsence-full.png" alt="Publicidade Full">
 					</div>
 				</div>
+			</div>
 
-				<div class="row">
-					<div class="col-m-4">
-						<div class="block__post--highlights">
-							<div class="block__post--image">
-								<img src="img/fke/post-image-2.png" alt="Faxina na vida">
-							</div>
-
-							<div class="block__post--category">
-								<a href="#this" class="btn btn-default">compras</a>
-							</div>
-
-							<div class="block__post--content">
-								<div class="block__post--share">
-									<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
-									<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
-								</div>
-
-								<div class="block__post--title">
-									<h2>Faxina na vida</h2>
-								</div>
-
-								<div class="block__post--time">
-									<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
-								</div>
-
-								<div class="block__post--entries">
-									<p>O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta, não te serve ou simplesmente você não quer mais. O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
-								</div>
+			<div class="center">
+				<div class="block__highlights">
+					<div class="row">
+						<div class="col-m-4">
+							<div class="block__adsence--medium">
+								<img src="img/fke/adsence-medium.png" alt="Publicidade Media">
 							</div>
 						</div>
-					</div>
 
-					<div class="col-m-4">
-						<div class="block__post--highlights">
-							<div class="block__post--category">
-								<a href="#this" class="btn btn-default">agenda</a>
-							</div>
-
-							<div class="row">
-								<div class="col-m-12">
-									<div class="block__calendar">
-										<div class="block__calendar--image">
-											<img src="img/fke/calendar-image-1.png" alt="Feira livre">
-										</div>
-
-										<div class="block__calendar--title">
-											<a href="#this">Feira livre</a>
-											<span>@ Eixão norte, 15h</span>
-										</div>
-
-										<div class="block__calendar--date">
-											<span>domingo</span>
-											<p>24 <br /> maio</p>
-										</div>
-									</div>
+						<div class="col-m-8">
+							<div class="block__post--highlights">
+								<div class="block__post--image">
+									<img src="img/fke/post-image-1.png" alt="Quinta é quase sexta">
 								</div>
 
-								<div class="col-m-12">
-									<div class="block__calendar">
-										<div class="block__calendar--image">
-											<img src="img/fke/calendar-image-2.png" alt="Picnik">
-										</div>
-
-										<div class="block__calendar--title">
-											<a href="#this">Picnik</a>
-											<span>@ Praça dos cristais, 15h</span>
-										</div>
-
-										<div class="block__calendar--date">
-											<span>segunda</span>
-											<p>25 <br /> maio</p>
-										</div>
-									</div>
+								<div class="block__post--category">
+									<a href="#this" class="btn btn-default">bebes</a>
 								</div>
 
-								<div class="col-m-12">
-									<div class="block__calendar">
-										<div class="block__calendar--image">
-											<img src="img/fke/calendar-image-3.png" alt="Feira livre">
-										</div>
-
-										<div class="block__calendar--title">
-											<a href="#this">Santuário apresenta...</a>
-											<span>@ SQN 214, 15h</span>
-										</div>
-
-										<div class="block__calendar--date">
-											<span>sexta</span>
-											<p>30 <br /> maio</p>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div class="block__actions">
-								<div class="row">
-									<div class="col-m-5">
-										<a href="#this" class="btn btn-white">sugerir evento</a>
+								<div class="block__post--content">
+									<div class="block__post--share">
+										<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+										<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
 									</div>
 
-									<div class="col-m-">
-										<a href="#this" class="btn btn-default">ver toda a agenda</a>
+									<div class="block__post--title">
+										<h2>Quinta é quase sexta</h2>
+									</div>
+
+									<div class="block__post--time">
+										<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>danicronemberger</strong></a></p>
+									</div>
+
+									<div class="block__post--entries">
+										<p>Em primeiro lugar, permita-me discorrer sobre o conceito que cunhei sobre música-boa. Segundo eu mesma, música-boa é aquela de que você gosta. Ponto. Aquela que você cresceu ouvindo. Ponto. Segundo eu mesma, música-boa é aquela de que você gosta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 
-					<div class="col-m-4">
-						<div class="block__post--highlights">
-							<div class="block__post--image">
-								<img src="img/fke/post-image-3.png" alt="Ana Marta, 65">
+					<div class="row">
+						<div class="col-m-4">
+							<div class="block__post--highlights">
+								<div class="block__post--image">
+									<img src="img/fke/post-image-2.png" alt="Faxina na vida">
+								</div>
+
+								<div class="block__post--category">
+									<a href="#this" class="btn btn-default">compras</a>
+								</div>
+
+								<div class="block__post--content">
+									<div class="block__post--share">
+										<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+										<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+									</div>
+
+									<div class="block__post--title">
+										<h2>Faxina na vida</h2>
+									</div>
+
+									<div class="block__post--time">
+										<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+									</div>
+
+									<div class="block__post--entries">
+										<p>O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta, não te serve ou simplesmente você não quer mais. O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+									</div>
+								</div>
 							</div>
+						</div>
 
-							<div class="block__post--category">
-								<a href="#this" class="btn btn-default">3x4</a>
+						<div class="col-m-4">
+							<div class="block__post--highlights">
+								<div class="block__post--category">
+									<a href="#this" class="btn btn-default">agenda</a>
+								</div>
+
+								<div class="row block__post--highlights-calendar">
+									<div class="col-m-12">
+										<div class="block__calendar">
+											<div class="block__calendar--image">
+												<img src="img/fke/calendar-image-1.png" alt="Feira livre">
+											</div>
+
+											<div class="block__calendar--title">
+												<a href="#this">Feira livre</a>
+												<span>@ Eixão norte, 15h</span>
+											</div>
+
+											<div class="block__calendar--date">
+												<span>domingo</span>
+												<p>24 <br /> <strong>maio</strong></p>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-12">
+										<div class="block__calendar">
+											<div class="block__calendar--image">
+												<img src="img/fke/calendar-image-2.png" alt="Picnik">
+											</div>
+
+											<div class="block__calendar--title">
+												<a href="#this">Picnik</a>
+												<span>@ Praça dos cristais, 15h</span>
+											</div>
+
+											<div class="block__calendar--date">
+												<span>segunda</span>
+												<p>25 <br /> <strong>maio</strong></p>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-12">
+										<div class="block__calendar">
+											<div class="block__calendar--image">
+												<img src="img/fke/calendar-image-3.png" alt="Feira livre">
+											</div>
+
+											<div class="block__calendar--title">
+												<a href="#this">Santuário apresenta...</a>
+												<span>@ SQN 214, 15h</span>
+											</div>
+
+											<div class="block__calendar--date">
+												<span>sexta</span>
+												<p>30 <br /> <strong>maio</strong></p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="block__actions">
+									<div class="row">
+										<div class="col-m-5">
+											<a href="#this" class="btn btn-full btn-white">sugerir evento</a>
+										</div>
+
+										<div class="col-m-7">
+											<a href="#this" class="btn btn-full btn-default">ver toda a agenda</a>
+										</div>
+									</div>
+								</div>
 							</div>
+						</div>
 
-							<div class="block__post--content">
-								<div class="block__post--share">
-									<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
-									<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+						<div class="col-m-4">
+							<div class="block__post--highlights">
+								<div class="block__post--image">
+									<img src="img/fke/post-image-3.png" alt="Ana Marta, 65">
 								</div>
 
-								<div class="block__post--title">
-									<h2>Ana Marta, 65</h2>
+								<div class="block__post--category">
+									<a href="#this" class="btn btn-default">3x4</a>
 								</div>
 
-								<div class="block__post--time">
-									<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
-								</div>
+								<div class="block__post--content">
+									<div class="block__post--share">
+										<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+										<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+									</div>
 
-								<div class="block__post--entries">
-									<p>“ I’d try telling myself to quit being weak, and to snap out of it” “ I’d try telling myself to quit being weak, and to snap out of it”. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+									<div class="block__post--title">
+										<h2>Ana Marta, 65</h2>
+									</div>
+
+									<div class="block__post--time">
+										<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+									</div>
+
+									<div class="block__post--entries">
+										<p>“ I’d try telling myself to quit being weak, and to snap out of it” “ I’d try telling myself to quit being weak, and to snap out of it”. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+									</div>
 								</div>
 							</div>
 						</div>
