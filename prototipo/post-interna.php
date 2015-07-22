0b8202ee8500384bbbf5b248cbde6c51fb5ed48a
Mateus Moura <chagas.mateus@gmail.com>
2015-07-21 23:34:00 -0300

    development internal post page. 📄

---
+++ prototipo/post-interna.php
@@ -0,0 +1,81 @@
+<?php include("inc/header.php"); ?>
+
+			<main role="main" class="internal">
+				<section class="block__post">
+					<div class="center">
+						<div class="row">
+							<div class="col-m-12">
+								<div class="block__post--title">
+									<h1>Há flores por todos os lados</h1>
+
+									<div class="block__post--category align-right">
+										<a href="#this" class="btn btn-default">compras</a>
+									</div>
+
+									<div class="block__post--time">
+										<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>danicronemberger</strong></a></p>
+
+										<div class="block__post--share">
+											<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+											<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+										</div>
+									</div>
+								</div>
+
+								<div class="block__gallery">
+									<div class="row unicelular align-center">
+										<div class="col-m-5">
+											<img src="img/fke/gallery-1.png" alt="Galeria 1">
+										</div>
+										<div class="col-m-5">
+											<img src="img/fke/gallery-2.png" alt="Galeria 2">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white">
+						<div class="center">
+							<div class="block__post--content">
+								<div class="block__post--entries">
+									<p>Há muito tempo eu andava atrás da Moça do Buquê, sem saber que ela existia. Sei que flor é uma coisa linda de qualquer forma, mas sabe aquela vontade de presentear alguém com algo especial, fora daquele padrão floricultura que todo mundo conhece? Toda vez em que tive vontade de dar flores pra alguém, senti falta da Moça do Buquê. Sem saber que ela existia.</p>
+									<p>Primeiro, adorei a forma como ela chegou até a gente. O namorado da Moça foi quem mandou uma mensagem, apresentando o trabalho dela e ressaltando, com um orgulho discreto e bonito, a delicadeza de seus buquês.</p>
+									<p>Segundo, adorei o nome: Moça do Buquê. Não tem nome melhor pra ela, tem? E terceiro, a história por trás da Moça, que se chama Bruna Pelegio, tem 28 anos, e nasceu em Santana do Livramento, na fronteira com o Uruguai. Formada em contabilidade e trabalhando como analista de marketing, Bruna viu sua vida mudar há exatamente um ano.</p>
+									<p>Uma amiga pediu para ela fazer o buquê de seu casamento e lá foi Bruna, agarrada no buquê mais importante da sua vida, de Brasília... <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+								</div>
+
+								<div class="block__post--go">
+									<h4>Bora?</h4>
+									<p>Moça do Buquê</p>
+
+									<a href="tel:+6181414433">(61) 8141-4433</a> e <a href="mailTo:mocadobuque@gmail.com">mocadobuque@gmail.com</a></p>
+
+									<ul class="block__post--go-social">
+										<li>Instagram: <a href="#this">@moca_do_buque</a></li>
+										<li>Facebook: <a href="#this">@moca_do_buque</a></li>
+										<li>Twitter: <a href="#this">@moca_do_buque</a></li>
+									</ul>
+								</div>
+
+								<div class="block__adsence--post">
+									<div class="row unicelular align-center">
+										<div class="col-m-4">
+											<img src="img/fke/adsence-post.png">
+										</div>
+										<div class="col-m-4">
+											<img src="img/fke/adsence-post.png">
+										</div>
+										<div class="col-m-4">
+											<img src="img/fke/adsence-post.png">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+
+<?php include("inc/footer.php"); ?>
