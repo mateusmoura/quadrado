dba0ce8fc4f74f95e6ed1cd6406ebd1caaecdaab
Mateus Moura <chagas.mateus@gmail.com>
2015-07-22 21:53:31 -0300

    development search page 🔎

---
+++ prototipo/search-result.php
@@ -0,0 +1,319 @@
+<?php include("inc/header.php"); ?>
+
+			<main role="main" class="search">
+				<section class="block__post">
+					<div class="center">
+						<div class="row">
+							<div class="col-m-12">
+								<div class="block__post--title">
+									<h1>encontramos 125 posts dentro da categoria “compras”</h1>
+
+									<div class="block__post--category align-right">
+										<a href="#this" class="btn btn-default">ver todos os posts</a>
+									</div>
+
+									<div class="block__post--time"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="bg-white">
+						<div class="center">
+							<div class="block__highlights">
+								<div class="row">
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image">
+												<img src="img/fke/post-image-2.png" alt="Faxina na vida">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">compras</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Faxina na vida</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta, não te serve ou simplesmente você não quer mais. O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image">
+												<img src="img/fke/post-image-2.png" alt="Faxina na vida">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">compras</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Faxina na vida</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta, não te serve ou simplesmente você não quer mais. O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image">
+												<img src="img/fke/post-image-2.png" alt="Faxina na vida">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">compras</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Faxina na vida</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta, não te serve ou simplesmente você não quer mais. O melhor de mudar de casa é que você tem a oportunidade perfeita pra se livrar de tudo o que não presta. <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-m-4">
+										<div class="block__post--highlights">
+											<div class="block__post--image-auto">
+												<img src="img/fke/post-image-4.png" alt="Atenção: Nós vamos sobreviver ao pós-feriado">
+											</div>
+
+											<div class="block__post--category">
+												<a href="#this" class="btn btn-default">comes</a>
+											</div>
+
+											<div class="block__post--content">
+												<div class="block__post--share">
+													<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+													<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+												</div>
+
+												<div class="block__post--title">
+													<h2>Atenção: Nós vamos sobreviver ao pós-feriado</h2>
+												</div>
+
+												<div class="block__post--time">
+													<p>Em 5 de maio de 2015 por <a href="#this" class="btn btn-link"><strong>carolnogueira76</strong></a></p>
+												</div>
+
+												<div class="block__post--entries">
+													<p>Nós, do Quadrado, fazemos um trabalho beneficente Nós, do Quadrado, fazemos um trabalho beneficente. Nós, do Quadrado <a href="#this" class="btn btn-link"><i class="fa fa-arrow-right"></i></a></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="block__loading">
+								<div class="loading"></div>
+								<p>rapidão, estamos carregando mais resultados da sua busca</p>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+
+<?php include("inc/footer.php"); ?>
