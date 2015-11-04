76413d95ee2ea662ec0d15c5cb7514c5c5002632
Mateus Moura <chagas.mateus@gmail.com>
2015-11-04 13:55:10 -0200

    development search page.

--- projeto/wp-content/themes/quadrado/functions.php
+++ projeto/wp-content/themes/quadrado/functions.php
@@ -43,106 +43,106 @@ function register_my_menus() {
 
 function wp_custom_breadcrumbs() {
  
-  $showOnHome = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
-  $delimiter = '/'; // delimiter between crumbs
-  $home = 'Página inicial'; // text for the 'Home' link
-  $showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
-  $before = '<span class="current">'; // tag before the current crumb
-  $after = '</span>'; // tag after the current crumb
- 
-  global $post;
-  $homeLink = get_bloginfo('url');
- 
-  if (is_home() || is_front_page()) {
- 
+	$showOnHome = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
+	$delimiter = '/'; // delimiter between crumbs
+	$home = 'Página inicial'; // text for the 'Home' link
+	$showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
+	$before = '<span class="current">'; // tag before the current crumb
+	$after = '</span>'; // tag after the current crumb
+
+	global $post;
+	$homeLink = get_bloginfo('url');
+
+	if (is_home() || is_front_page()) {
+
 	if ($showOnHome == 1) echo '<div class="breadcrumb"><a href="' . $homeLink . '">' . $home . '</a></div>';
- 
-  } else {
- 
+
+	} else {
+
 	echo '<div class="breadcrumb"><a href="' . $homeLink . '">' . $home . '</a> ' . $delimiter . ' ';
- 
+
 	if ( is_category() ) {
-	  $thisCat = get_category(get_query_var('cat'), false);
-	  if ($thisCat->parent != 0) echo get_category_parents($thisCat->parent, TRUE, ' ' . $delimiter . ' ');
-	  echo $before . 'categoria "' . single_cat_title('', false) . '"' . $after;
- 
+		$thisCat = get_category(get_query_var('cat'), false);
+		if ($thisCat->parent != 0) echo get_category_parents($thisCat->parent, TRUE, ' ' . $delimiter . ' ');
+		echo $before . 'categoria "' . single_cat_title('', false) . '"' . $after;
+
 	} elseif ( is_search() ) {
-	  //echo $before . 'Busca por "' . get_search_query() . '"' . $after;
+		//echo $before . 'Busca por "' . get_search_query() . '"' . $after;
 		echo $before . 'Resultado de busca' . $after;
- 
+
 	} elseif ( is_day() ) {
-	  echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-	  echo '<a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a> ' . $delimiter . ' ';
-	  echo $before . get_the_time('d') . $after;
- 
+		echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+		echo '<a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a> ' . $delimiter . ' ';
+		echo $before . get_the_time('d') . $after;
+
 	} elseif ( is_month() ) {
-	  echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-	  echo $before . get_the_time('F') . $after;
- 
+		echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+		echo $before . get_the_time('F') . $after;
+
 	} elseif ( is_year() ) {
-	  echo $before . get_the_time('Y') . $after;
- 
+		echo $before . get_the_time('Y') . $after;
+
 	} elseif ( is_single() && !is_attachment() ) {
-	  if ( get_post_type() != 'post' ) {
-	  	$customTitle = get_the_title();
-	  	$customTitle = str_replace('/', '', $customTitle);
+		if ( get_post_type() != 'post' ) {
+			$customTitle = get_the_title();
+			$customTitle = str_replace('/', '', $customTitle);
 		$post_type = get_post_type_object(get_post_type());
 		$slug = $post_type->rewrite;
 		echo '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a>';
 		if ($showCurrent == 1) echo ' ' . $delimiter . ' ' . $before . $customTitle . $after;
-	  } else {
+		} else {
 		$cat = get_the_category(); $cat = $cat[0];
 		$cats = get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
 		if ($showCurrent == 0) $cats = preg_replace("#^(.+)\s$delimiter\s$#", "$1", $cats);
 		echo $cats;
 		if ($showCurrent == 1) echo $before . get_the_title() . $after;
-	  }
- 
+		}
+
 	} elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
-	  $post_type = get_post_type_object(get_post_type());
-	  echo $before . $post_type->labels->singular_name . $after;
- 
+		$post_type = get_post_type_object(get_post_type());
+		echo $before . $post_type->labels->singular_name . $after;
+
 	} elseif ( is_attachment() ) {
-	  $parent = get_post($post->post_parent);
-	  $cat = get_the_category($parent->ID); $cat = $cat[0];
-	  echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
-	  echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a>';
-	  if ($showCurrent == 1) echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
- 
+		$parent = get_post($post->post_parent);
+		$cat = get_the_category($parent->ID); $cat = $cat[0];
+		echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
+		echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a>';
+		if ($showCurrent == 1) echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
+
 	} elseif ( is_page() && !$post->post_parent ) {
-	  if ($showCurrent == 1) echo $before . get_the_title() . $after;
- 
+		if ($showCurrent == 1) echo $before . get_the_title() . $after;
+
 	} elseif ( is_page() && $post->post_parent ) {
-	  $parent_id  = $post->post_parent;
-	  $breadcrumbs = array();
-	  while ($parent_id) {
+		$parent_id  = $post->post_parent;
+		$breadcrumbs = array();
+		while ($parent_id) {
 		$page = get_page($parent_id);
 		$breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
 		$parent_id  = $page->post_parent;
-	  }
-	  $breadcrumbs = array_reverse($breadcrumbs);
-	  for ($i = 0; $i < count($breadcrumbs); $i++) {
+		}
+		$breadcrumbs = array_reverse($breadcrumbs);
+		for ($i = 0; $i < count($breadcrumbs); $i++) {
 		echo $breadcrumbs[$i];
 		if ($i != count($breadcrumbs)-1) echo ' ' . $delimiter . ' ';
-	  }
-	  if ($showCurrent == 1) echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
+		}
+		if ($showCurrent == 1) echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
 
 	} elseif ( is_tag() ) {
-	  echo $before . 'Posts tagged "' . single_tag_title('', false) . '"' . $after;
+		echo $before . 'Posts tagged "' . single_tag_title('', false) . '"' . $after;
 
 	} elseif ( is_author() ) {
-	   global $author;
-	  $userdata = get_userdata($author);
-	  echo $before . 'Articles posted by ' . $userdata->display_name . $after;
+		 global $author;
+		$userdata = get_userdata($author);
+		echo $before . 'Articles posted by ' . $userdata->display_name . $after;
 
 	} elseif ( is_404() ) {
-	  echo $before . 'Error 404' . $after;
+		echo $before . 'Error 404' . $after;
 	}
 
 	if ( get_query_var('paged') ) {
-	  if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
-	  echo __('Page') . ' ' . get_query_var('paged');
-	  if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
+		if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
+		echo __('Page') . ' ' . get_query_var('paged');
+		if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
 	}
 
 	echo '</div>';
@@ -151,19 +151,29 @@ function wp_custom_breadcrumbs() {
 } // end wp_custom_breadcrumbs()
 
 function update_value() {
-  // Set variables
-  $input_test = $_POST['newValue'];
-  $curso_id = $_POST['cursoId'];
-  $input_vendido = $_POST['vendido'];
+	// Set variables
+	$input_test = $_POST['newValue'];
+	$curso_id = $_POST['cursoId'];
+	$input_vendido = $_POST['vendido'];
 
-  // Check variables for fallbacks
-  if (!isset($input_test) || $input_test == "") { $input_test = "Fall Back"; }
-  // Update the field
-  update_field('quantidade_de_alunos', $input_test, $curso_id);
-  update_field('total_de_vendas', $input_vendido, $curso_id);
+	// Check variables for fallbacks
+	if (!isset($input_test) || $input_test == "") { $input_test = "Fall Back"; }
+	// Update the field
+	update_field('quantidade_de_alunos', $input_test, $curso_id);
+	update_field('total_de_vendas', $input_vendido, $curso_id);
 }
 add_action( 'wp_ajax_nopriv_update_value',  'update_value' );
 add_action( 'wp_ajax_update_value','update_value' );
+
+
+// search filter
+function fb_search_filter($query) {
+	if ( !$query->is_admin && $query->is_search) {
+		$query->set('post_type', array('artes', 'eu-acho', 'feiras', 'mesa', 'passeio', 'pessoas', 'pistas', 'sacolas') ); // id of page or post
+	}
+	return $query;
+}
+add_filter( 'pre_get_posts', 'fb_search_filter' );
 
 
 add_theme_support( "post-thumbnails" );
--- projeto/wp-content/themes/quadrado/search.php
+++ projeto/wp-content/themes/quadrado/search.php
@@ -2,23 +2,11 @@
 
 <?php $posts_types = array('artes', 'eu-acho', 'feiras', 'mesa', 'passeio', 'pessoas', 'pistas', 'sacolas'); ?>
 
-<?php
-	// global $query_string;
-
-	// $query_args = explode("&", $query_string);
-	// $search_query = array( 'post_type' => 'post' );
-
-	// foreach($query_args as $key => $string) {
-	// 	$query_split = explode("=", $string);
-	// 	$search_query[$query_split[0]] = urldecode($query_split[1]);
-	// }
-
-	// //var_dump($search_query);
-
-	// $search = new WP_Query(array('post_type' => $posts_types));
-
-	// var_dump($search);
-?>
+		<!-- IMAGEM DE BACKGROUND EM DESTAQUE -->
+		<!-- <div class="header__image--post header__image--post-random">
+			<img src="<?php echo $imagem_de_fundo['url'] ?>" class="header__image--post-preloading" alt="<?php echo $imagem_de_fundo['title'] ?>">
+			<img src="<?php echo $imagem_de_fundo['url'] ?>" alt="<?php echo $imagem_de_fundo['title'] ?>">
+		</div> -->
 
 			<main role="main" class="search">
 				<section class="block__post">
@@ -43,65 +31,66 @@
 							<div class="block__highlights">
 								<?php if (have_posts()) : while (have_posts()) : setup_postdata( the_post() );
 									$postID			= get_the_ID();
-									$date			= get_the_date('d \d\e F \d\e Y', $postID);;
+									$date			= get_the_date('d \d\e F \d\e Y', $postID);
+									$postType		= get_post_type($postID);
+
+									//if ( $postType != 'page' && $postType != 'lugares' ):
 								?>
 
-								<?php if( $counter % 3 == 0 ) { ?>
-									
-									<div class="row">
-								<?php } ?>
-										<div class="col-m-4">
-											<div class="block__post--highlights">
-												<div class="block__post--image-auto">
-													<?php the_post_thumbnail(); ?>
-												</div>
-
-												<div class="block__post--category">
-													<a href="<?php bloginfo('url'); ?>/<?php echo get_post_type($postID); ?>" class="btn btn-default"><?php echo get_post_type($postID); ?></a>
-												</div>
-
-												<div class="block__post--content">
-													<div class="block__post--share">
-														<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
-														<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+										<?php if( $counter % 3 == 0 ) { ?>
+										<div class="row">
+										<?php } ?>
+											<div class="col-m-4">
+												<div class="block__post--highlights">
+													<div class="block__post--image-auto">
+														<?php the_post_thumbnail(); ?>
 													</div>
 
-													<div class="block__post--title">
-														<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+													<div class="block__post--category">
+														<a href="<?php bloginfo('url'); ?>/<?php echo $postType; ?>" class="btn btn-default"><?php echo $postType; ?></a>
 													</div>
 
-													<div class="block__post--time">
-														<p>Em <?php echo $date; ?> por <a href="#this" class="btn btn-link"><strong><?php the_author(); ?></strong></a></p>
-													</div>
+													<div class="block__post--content">
+														<div class="block__post--share">
+															<a href="#this" class="btn btn-link"><i class="fa fa-comment"></i> 23</a>
+															<a href="#this" class="btn btn-link"><i class="fa fa-share"></i> 1k</a>
+														</div>
 
-													<div class="block__post--entries">
-														<?php the_excerpt(); ?>
+														<div class="block__post--title">
+															<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+														</div>
+
+														<div class="block__post--time">
+															<p>Em <?php echo $date; ?> por <a href="#this" class="btn btn-link"><strong><?php the_author(); ?></strong></a></p>
+														</div>
+
+														<div class="block__post--entries">
+															<?php the_excerpt(); ?>
+														</div>
 													</div>
 												</div>
 											</div>
+										<?php if( $page_index > 1 ) { ?>
 										</div>
-								<?php if( $page_index > 1 ) { ?>
-									</div>
-								<?php $page_index = 0; } else {
-									++$page_index;
-								} 
+										<?php $page_index = 0; } else {
+											++$page_index;
+										} 
 
-								++$counter;
-								?>
+										++$counter;
+										?>
+									<?php //endif; ?>
 								<?php endwhile; ?>
 								</div>
 								<?php posts_nav_link(' &#8212; ', __('&laquo; P&aacute;gina anterior'), __('Pr&oacute;xima p&aacute;gina &raquo;')); ?>
 								<?php else: ?>
-									<p>Não foi encontrado nenhum resultado para <?php echo get_search_query(); ?>. <a href="<?php bloginfo('url'); ?>/cursos/">Volte aos cursos</a></p>
+									<p>Não foi encontrado nenhum resultado para "<?php echo get_search_query(); ?>". <a href="<?php bloginfo('url'); ?>/cursos/">Ver todos os posts</a></p>
 								<?php endif; ?>
 							</div>
 
-							
-
-							<div class="block__loading">
+							<!-- <div class="block__loading">
 								<div class="loading"></div>
 								<p>rapidão, estamos carregando mais resultados da sua busca</p>
-							</div>
+							</div> -->
 						</div>
 					</div>
 				</section>
