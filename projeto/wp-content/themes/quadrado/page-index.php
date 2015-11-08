2a133ef76630d38550fce1cd88036f4d08656c12
Mateus Moura <chagas.mateus@gmail.com>
2015-11-08 17:35:07 -0200

    added google Maps home page.

--- projeto/wp-content/themes/quadrado/page-index.php
+++ projeto/wp-content/themes/quadrado/page-index.php
@@ -1,6 +1,55 @@
 <?php get_header(); ?>
 
 <?php 
+	$counter				= 0;
+	$page_index				= 0;
+	$posts_types			= array('lugares');
+
+	$places					= query_posts( array( 'post_type' => $posts_types, 'posts_per_page' => '150', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+
+	for ($i = 0; $i < count($places); ++$i) {
+		$lat_long										= get_field('localizacao', $places[$i]->ID);
+		$link_do_post									= get_field('link_do_post_sobre_o_lugar', $places[$i]->ID);
+		$imagem											= get_field('imagem', $places[$i]->ID);
+		$horario_de_funcionamento						= get_field('horario_de_funcionamento', $places[$i]->ID);
+		$categories										= wp_get_post_categories($places[$i]->ID);
+		$cats											= array();
+
+		foreach($categories as $c){
+			$cat										= get_category( $c );
+			$category_link								= get_category_link($c);
+			$cats[]										= array( 'name' => $cat->name, 'slug' => $cat->slug, 'permalink' => esc_url( $category_link ) );
+		}
+
+		$places[$i]->localizacao						= $lat_long;
+		$places[$i]->link_do_post						= $link_do_post;
+		$places[$i]->imagem								= $imagem;
+		$places[$i]->horario_de_funcionamento			= $horario_de_funcionamento;
+		$places[$i]->categories							= $cats;
+	}
+
+	$json_pages				= json_encode($places);
+
+	wp_reset_query();
+?>
+
+		<script>
+			var places_data = <?php echo $json_pages; ?>;
+
+			function ready(fn) {
+				if (document.readyState != 'loading'){
+					fn();
+				} else {
+					document.addEventListener('DOMContentLoaded', fn);
+				}
+			}
+
+			ready(function () {
+				MM.GoogleMaps(document.getElementById('google__map'), places_data);
+			});
+		</script>
+
+<?php 
 	$posts_types = array('artes', 'eu-acho', 'feiras', 'mesa', 'passeio', 'pessoas', 'pistas', 'sacolas'); 
 	$recent_posts_count = 0;
 ?>
